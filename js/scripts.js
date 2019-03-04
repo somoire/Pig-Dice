236b5b88edea5d94aa4008d884bc61670e52520c
Rodney Somoire <rayr30667@gmail.com>
2019-03-04 00:53:55 -0500

    Mdified part

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// business logic
+var firstPlayer;
+var secondPlayer;
+
+var rollDice=function() {
+  return
+}
+
+function player(first turn) {
+  this.roll = 0;
+  this.rollingScore = 0;
+  this.finalscore = 0;
+  this.turn = nextplayer;
+  this.player2;
+}
+
+player1.prototype.rollone = function
